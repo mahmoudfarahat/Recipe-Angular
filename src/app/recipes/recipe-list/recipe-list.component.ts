49c19e91af0f56c0be8664d355333fd7d862026e
Mahmoud Farahat <Mahmoud.Farahat@corp.arab-computers.com>
2022-08-25 16:05:10 +0200

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Recipe } from './../recipe.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes : Recipe[] = [
+    new Recipe('A Test Recipe','this is a simply a test','https://www.simplyrecipes.com/thmb/mbN8mXZ0srgAT1YrDU61183t0uM=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg')
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
