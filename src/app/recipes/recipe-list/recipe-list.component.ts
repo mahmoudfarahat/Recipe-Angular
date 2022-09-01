cef4393d5dd59df78231fea8fa051f3a161b96f0
Mahmoud Farahat <Mahmoud.Farahat@corp.arab-computers.com>
2022-09-01 14:36:33 +0200

    1.1

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,6 +10,9 @@ export class RecipeListComponent implements OnInit {
 
   recipes : Recipe[] = [
     new Recipe('A Test Recipe','this is a simply a test','https://www.simplyrecipes.com/thmb/mbN8mXZ0srgAT1YrDU61183t0uM=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg')
+  ,
+  new Recipe('A Test Recipe','this is a simply a test','https://www.simplyrecipes.com/thmb/mbN8mXZ0srgAT1YrDU61183t0uM=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg')
+
   ]
   constructor() { }
 
