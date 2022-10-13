721f2725fa338e750b26fcf40a1934cc2c5acd03
Mahmoud Farahat <Mahmoud.Farahat@corp.arab-computers.com>
2022-10-13 15:11:28 +0200

    1.2

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,17 +1,19 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from '../recipes/recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
-  constructor() { }
+  constructor(private shoppingListService:ShoppingListService) { }
 
 recipeSelected = new EventEmitter<Recipe>()
 
   private recipes : Recipe[] = [
+    
     new Recipe('A Test Recipe',
     'this is a simply a test',
     'https://www.simplyrecipes.com/thmb/mbN8mXZ0srgAT1YrDU61183t0uM=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg',
@@ -31,5 +33,10 @@ recipeSelected = new EventEmitter<Recipe>()
 
   getRecipes(){
     return this.recipes.slice()
+  }
+
+
+  addIngredientToShoppingList(ingredient:Ingredient []){
+    this.shoppingListService.addIngredients(ingredient)
   }
 }
--- src/app/services/shopping-list.service.ts
+++ src/app/services/shopping-list.service.ts
@@ -30,5 +30,11 @@ this.ingredientsChanged.emit(this.ingredients.slice())
     
   }
 
+  addIngredients(ingredients: Ingredient[]){
+ 
+    this.ingredients.push(...ingredients)
+    this.ingredientsChanged.emit(this.ingredients.slice())
+}
+
 
 }
